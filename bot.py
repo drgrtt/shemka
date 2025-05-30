538ba899c8d3bc7b8fba54a7a3b6c4e4c3bebb59
drgrtt <161079405+drgrtt@users.noreply.github.com>
2025-05-30 12:44:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import json
 import os
 from aiogram import Bot, Dispatcher, executor, types
 
-TOKEN = "8044456789:AAHZfhB5E16coRP__7lmj5dBQazyh-4N7K8"
+TOKEN = "8044456789:AAFovBE8r5_oKOX_llUhqM0sMRk83pnYi1k"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
